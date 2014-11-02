08e328914e4421b4284382dd9c6e69b0c16c1223
ThomPoillot <thomas.poillot@gmail.com>
2014-11-02 18:23:02 +0100

    quelques tests sur les parcours de repertoires

--- connection.py
+++ connection.py
@@ -3,6 +3,7 @@ import thread
 import time
 import Queue
 import sys
+import os
 
 class Ftp:
     #lala
@@ -20,15 +21,59 @@ class Ftp:
         connect.storbinary('STOR ' + "name",  file)
         file.close()
         
-if __name__ == '__main__':
-    print "hello"
-    maConnection = Ftp(sys.argv[1],  sys.argv[2], sys.argv[3] )
-    print "hello"
-    connect = maConnection.connect()
+        
+        
+        #La fonction os.walk(path) cree un generateur de triplets (root, dirs, files) dans l'arborescence de path. 
+#Un triplet est genere par repertoire visite. root represente le chemin d'acces du repertoire
+#visite. dirs est la liste des sous-repertoires du repertoire root et files est la liste des fichiers du
+#repertoire root. 
+def walkTroughLocalDirectory(path):  
+    size = 0  
+    print "nombre de fichier total dans le dossier =  %s" % len(os.listdir(path))
+    for root, dirs, files in os.walk(path):  
+        for fic in files:  
+            size += os.path.getsize(os.path.join(root, fic)) 
+    return size 
+  
 
+def doesLocalFolderExists(path):
+    current_dir = os.path.dirname(path)
+    print current_dir
     
+def walkThroughServer(connect,  path):
+#acces au repertoire
+#FONCTIONNE COMME UN SERVEUR LINUX 
+
+    connect.cwd(path)
+    print connect.pwd()
+    rep = connect.dir() # on recupere le listing
+    
+def synchroFTP(connect,  localPath, serverPath):
+    for root, dirs, files in os.walk(localPath):  
+        for fichier in files:
+            
+            #envoiFichierSurFtp(connect,  filePath)
+            filePath = root + "\\"+ fichier
+            print filePath
+           
+    
+    
+if __name__ == '__main__':
+
+    if os.path.isdir(sys.argv[5]) : 
+        print "le repertoire : " + sys.argv[5] + " existe"
+        maConnection = Ftp(sys.argv[1],  sys.argv[2], sys.argv[3] )
+        print walkTroughLocalDirectory(sys.argv[4])
+        connect = maConnection.connect()
+    else :
+        print "le repertoire : " + sys.argv[5] + " n\'existe pas, operation annulee"
+
+    synchroFTP(connect, sys.argv[4],   "python")
    # maConnection.sendFile("C:\Users\ISEN\Desktop\pompozob.txt")
-    connect.retrlines('LIST')
+   # tableau =  connect.retrlines('LIST')
+    #connect.dir('LIST')
+    #print tableau[(0, 0)]
+    
 
 
  #Define a function for the thread
